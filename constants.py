3d2115a4fe03921235fa382b3248fa3d1a1a1764
cyrus01337 <cyrus21337@gmail.com>
2020-06-09 05:55:01 +0100

    Reorganise & compartmentalise

---
+++ constants.py
@@ -0,0 +1,7 @@
+"""
+Unaltered variables with immutable values
+"""
+PREFIX = "5!"
+NO_CLASS = (f"You don't have a class yet, but we can fix that! You can view "
+            f"the available classes by doing `{PREFIX}classes`, follow the "
+            f"instructions to pick a class!")
